6adb625f9063305643d9ff52a127a905052f178e
Fionn Malone <fmalone@google.com>
2023-02-28 05:51:41 +0000

    Add exact GC reference DM.

--- density_matrix.py
+++ density_matrix.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import itertools
 import numpy as np
 from typing import List, Tuple, Union
 
@@ -10,7 +11,7 @@ def fermi_factor(ek, mu, beta):
 
 
 def compute_electron_number(mu, eigs, beta):
-    return sum(fermi_factor(eigs, mu, beta))
+    return 2 * sum(fermi_factor(eigs, mu, beta))
 
 
 def cost_function(mu, eigs, beta, target_electron_number):
@@ -26,26 +27,64 @@ def find_chemical_potential(eigs, beta, target_num_elec, mu0=0.0):
 
 @dataclass
 class DensityMatrix:
-    num_basis: int
+    num_spin_orbs: int
     occ_strings: List[np.ndarray]
     weights: np.ndarray
 
     @staticmethod
     def build_grand_canonical(fermi_occupations: np.ndarray, num_samples: int):
         occ_string = []
-        orb_indices = np.arange(len(fermi_occupations), dtype=np.int32)
-        random_numbers = np.random.random(
-            size=num_samples * len(fermi_occupations)
-        ).reshape((num_samples, len(fermi_occupations)))
+        # Use spin orbitals abab ordering
+        num_spin_orbs = 2 * len(fermi_occupations)
+        orb_indices = np.arange(num_spin_orbs, dtype=np.int32)
+        random_numbers = np.random.random(size=num_samples * num_spin_orbs).reshape(
+            (num_samples, num_spin_orbs)
+        )
+        fermi_spin_orb = np.zeros(num_spin_orbs)
+        fermi_spin_orb[::2] = fermi_occupations
+        fermi_spin_orb[1::2] = fermi_occupations
         for isample in range(num_samples):
             pi = random_numbers[isample]
-            occ_str = orb_indices[np.where(fermi_occupations > pi)]
+            occ_str = orb_indices[np.where(fermi_spin_orb > pi)]
             occ_string.append(occ_str)
 
         return DensityMatrix(
-            num_basis=len(fermi_occupations),
+            num_spin_orbs=num_spin_orbs,
             occ_strings=occ_string,
             weights=np.ones(num_samples),
+        )
+
+    @staticmethod
+    def build_grand_canonical_exact(eigenvalues: np.ndarray, mu: float, beta: float):
+        occ_string = []
+        weights = []
+        # Use spin orbitals abab ordering
+        num_spin_orbs = 2 * len(eigenvalues)
+        orb_indices = np.arange(num_spin_orbs, dtype=np.int32)
+        spin_eig = np.zeros(num_spin_orbs)
+        spin_eig[::2] = eigenvalues
+        spin_eig[1::2] = eigenvalues
+        Zgc = 0
+        for nelec in range(0, num_spin_orbs+1):
+            for occ_str in itertools.combinations(range(0, num_spin_orbs), nelec):
+                boltzmann = np.exp(-beta*(sum(spin_eig[list(occ_str)])-mu*nelec))
+                Zgc += boltzmann
+                weights.append(np.array(boltzmann))
+                occ_string.append(np.array(occ_str))
+
+        # dimh = 0
+        # import math
+        # nmo = len(fermi_occupations)
+        # for na in range(nmo+1):
+        #     for nb in range(nmo+1):
+        #         dimh += math.comb(nmo, na)*math.comb(nmo, nb)
+
+        #print(len(occ_string), dimh)
+
+        return DensityMatrix(
+            num_spin_orbs=num_spin_orbs,
+            occ_strings=occ_string,
+            weights=len(occ_string)*np.array(weights)/Zgc,
         )
 
     @property
@@ -54,9 +93,13 @@ class DensityMatrix:
 
     def compute_occupations(self) -> Tuple[float, float]:
         """Compute average electron number from statistical sample of density matrix"""
-        occupations = np.zeros((self.num_samples, self.num_basis))
+        occupations = np.zeros((self.num_samples, self.num_spin_orbs))
         for isample in range(self.num_samples):
-            occupations[isample, self.occ_strings[isample]] += 1.0 * self.weights[isample]
+            if len(self.occ_strings[isample]) == 0:
+                continue
+            occupations[isample, self.occ_strings[isample]] += (
+                1.0 * self.weights[isample]
+            )
         return (
             np.mean(occupations, axis=0),
             np.std(occupations, ddof=1, axis=0) / (self.num_samples**0.5),
@@ -68,12 +111,19 @@ class DensityMatrix:
         """Compute average electron number from statistical sample of density matrix"""
         one_body = np.zeros((self.num_samples,))
         for isample in range(self.num_samples):
-            one_body[isample] += sum(matrix_elements[self.occ_strings[isample]]*self.weights[isample])
+            if len(self.occ_strings[isample]) == 0:
+                continue
+            one_body[isample] += sum(
+                matrix_elements[self.occ_strings[isample] // 2] * self.weights[isample]
+            )
         return float(np.mean(one_body)), np.std(one_body, ddof=1) / (
             self.num_samples**0.5
         )
 
     def compute_electron_number(self) -> Tuple[float, float]:
         """Compute average electron number from statistical sample of density matrix"""
-        nav = [len(occ_str)*weight for occ_str, weight in zip(self.occ_strings, self.weights)]
+        nav = [
+            len(occ_str) * weight
+            for occ_str, weight in zip(self.occ_strings, self.weights)
+        ]
         return float(np.mean(nav)), np.std(nav, ddof=1) / (self.num_samples**0.5)
--- density_matrix_test.py
+++ density_matrix_test.py
@@ -22,7 +22,7 @@ def test_chemical_potential():
 def test_sampling_grand_canonical():
     target_num_elec = 14
     system = UEG.build(target_num_elec, 1.0, 10)
-    theta = 1.0
+    theta = 0.125
     beta = system.calc_beta_from_theta(theta)
     mu = find_chemical_potential(system.eigenvalues, beta, system.num_elec)
     occs = fermi_factor(system.eigenvalues, mu, beta)
@@ -30,35 +30,31 @@ def test_sampling_grand_canonical():
     np.random.seed(7)
     dm = DensityMatrix.build_grand_canonical(occs, num_samples)
     nav, nav_err = dm.compute_electron_number()
-    # A bit dodgy using atol = std_err as only expected to be true ~ 0.6 percent
-    # of the time but for this seed it turns out to be ok.
-    assert np.isclose(nav, target_num_elec, atol=nav_err)
+    assert np.isclose(nav, target_num_elec, atol=0.02)
     fermi, fermi_err = dm.compute_occupations()
-    assert np.allclose(fermi, occs, atol=0.02)
+    assert np.allclose(fermi[::2], occs, atol=0.02)
+    assert np.allclose(fermi[1::2], occs, atol=0.02)
     kinetic_energy, kinetic_energy_err = dm.contract_diagonal_one_body(
         system.eigenvalues
     )
-    reference_energy = sum(system.eigenvalues * occs)
-    assert np.isclose(kinetic_energy, reference_energy, atol=kinetic_energy_err)
+    reference_energy = 2 * sum(system.eigenvalues * occs)
+    assert np.isclose(kinetic_energy, reference_energy, atol=2*kinetic_energy_err)
 
 def test_exact_grand_canonical():
-    target_num_elec = 14
-    system = UEG.build(target_num_elec, 1.0, 10)
+    target_num_elec = 4
+    system = UEG.build(target_num_elec, 1.0, 0.5)
     theta = 1.0
     beta = system.calc_beta_from_theta(theta)
     mu = find_chemical_potential(system.eigenvalues, beta, system.num_elec)
     occs = fermi_factor(system.eigenvalues, mu, beta)
-    num_samples = 10000
-    np.random.seed(7)
-    dm = DensityMatrix.build_grand_canonical(occs, num_samples)
+    dm = DensityMatrix.build_grand_canonical_exact(system.eigenvalues, mu, beta)
     nav, nav_err = dm.compute_electron_number()
-    # A bit dodgy using atol = std_err as only expected to be true ~ 0.6 percent
-    # of the time but for this seed it turns out to be ok.
     assert np.isclose(nav, target_num_elec, atol=nav_err)
     fermi, fermi_err = dm.compute_occupations()
-    assert np.allclose(fermi, occs, atol=0.02)
+    assert np.allclose(fermi[::2], occs)
+    assert np.allclose(fermi[1::2], occs)
     kinetic_energy, kinetic_energy_err = dm.contract_diagonal_one_body(
         system.eigenvalues
     )
-    reference_energy = sum(system.eigenvalues * occs)
-    assert np.isclose(kinetic_energy, reference_energy, atol=kinetic_energy_err)
+    reference_energy = 2 * sum(system.eigenvalues * occs)
+    assert np.isclose(kinetic_energy, reference_energy, atol=2*kinetic_energy_err)
