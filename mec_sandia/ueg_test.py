d57a0f580baf78a1604b1ac7f71b5f63b152891d
Fionn Malone <fmalone@google.com>
2023-03-01 05:49:57 +0000

    Attempt at resources.

--- mec_sandia/ueg_test.py
+++ mec_sandia/ueg_test.py
@@ -2,7 +2,8 @@ import numpy as np
 
 from mec_sandia.ueg import UEG
 
+
 def test_build():
     ueg_inst = UEG.build(14, 1.0, 10)
     assert len(ueg_inst.eigenvalues) == 389
-    assert np.isclose(sum(ueg_inst.eigenvalues[:7]), 6*ueg_inst.eigenvalues[1])
+    assert np.isclose(sum(ueg_inst.eigenvalues[:7]), 6 * ueg_inst.eigenvalues[1])
