93f21242635254ba8a611eddfa09a6eda02746f2
Fionn Malone <fmalone@google.com>
2023-04-06 17:28:15 +0000

    Updates for correctness.

--- notebooks/plot_gaussian_conv.py
+++ notebooks/plot_gaussian_conv.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from pyscf.lib.numpy_helper import cartesian_prod
 
 import matplotlib.pyplot as plt
+
 plt.rcParams["font.family"] = "sans-serif"
 plt.rcParams["font.sans-serif"] = "arial"
 plt.rcParams["text.usetex"] = True
@@ -13,50 +14,119 @@ plt.rcParams["mathtext.fontset"] = "stix"
 colors = ["#4285f4", "#ea4335", "#fbbc04", "#34a853"]
 
 # Check convergence wrt PW cutoff
-v_proj = 4.0 # atomic units just taken from carbon
+v_proj = 4.0  # atomic units just taken from carbon
 mass_proj = 1836
-ke = 0.5 * mass_proj * v_proj**2.0 # classical ke
-kproj = np.array([mass_proj*v_proj, 0, 0])
+ke = 0.5 * mass_proj * v_proj**2.0  # classical ke
+kproj = np.array([mass_proj * v_proj, 0, 0])
+kproj[0] = 0.0
 sigma_k = 4.0
 
+
 def get_ngmax(ecut, box_length):
-    ng_max = math.ceil(np.sqrt(2*ecut)/(2*np.pi/box_length))
-    if ng_max % 2 == 0:
-        ng_max += 1
+    ng_max = math.ceil(np.sqrt(2 * ecut) / (2 * np.pi / box_length))
+    # if ng_max % 2 == 0:
+    #     ng_max += 1
     return ng_max
 
-L_bohr = 15
-import pandas as pd
-df = pd.DataFrame({"cutoffs": [1000, 10000, 5e4, 1e5]}) 
-for sigma_k in [4.0]:
-    results = []
-    for ecut_ev in df.cutoffs.values: 
-        ecut_ha = (ecut_ev/Hartree)
-        nmax = get_ngmax(ecut_ha, L_bohr)
-        norm = 0.0
-        sum_k = 0.0
-        #for ni, nj, nk in itertools.product(range(-nmax, nmax), repeat=3):
-        kgrid = np.arange(-nmax/2, nmax/2)
-        kxyz_grid = (2*np.pi/L_bohr) * cartesian_prod([kgrid, kgrid, kgrid])
-        kpsq = np.sum((kxyz_grid+kproj[None,:])**2.0, axis=-1)
-        ksq = np.sum(kxyz_grid**2.0, axis=-1)
-        prefactor = (np.sqrt(2*np.pi)/(sigma_k*L_bohr))**3.0
-        sum_k = np.sum(kpsq*np.exp(-ksq/(2*sigma_k**2.0)))
-        norm = np.sum(np.exp(-ksq/(2*sigma_k**2.0)))
-        ke_sum = sum_k*prefactor/(2*mass_proj) - ke
-        ke_int = 3*sigma_k**2.0/(2*mass_proj)
-        print(ke_sum, ke_int, ke_sum-ke_int)
-        results.append((ke_sum-ke_int))
-    df[f"sigma_{sigma_k}"] = results
 
-# for i, sigma_k in enumerate([4.0, 6.0, 10.0][::-1]):
-#     sigma_d = int(sigma_k)
-#     plt.plot(df.cutoffs, np.abs(df[f"sigma_{sigma_k}"]), marker="o", label=f"$\sigma_k$ = {sigma_d}", color=colors[i])
-# plt.tick_params(which="both", labelsize=14, direction="in")
-# plt.legend(frameon=False, loc="upper right", fontsize=14)
-# plt.axhline(1e-4, ls=':', color="grey")
-# plt.xlabel(r"$E_{\mathrm{cut}} (\mathrm{eV})$", fontsize=14)
-# plt.yscale("log")
-# plt.xscale("log")
-# plt.ylabel("Kinetic Energy Error (Ha)", fontsize=14)
-# plt.savefig("cutoff_convergence.pdf", bbox_inches="tight", dpi=300)
+L_bohr = 15
+from dataclasses import dataclass, field
+from typing import List
+
+
+nx_grid = 2**np.arange(2, 10)  # quantum algorithm takes things as a power of 2
+ke_cutoffs_eV = 0.5 * (2 * np.pi)**2 * nx_grid**2 / L_bohr**2 * 27.11 # highest energy components in eV
+
+@dataclass
+class Results:
+    cutoffs: List = field(
+        default_factory=lambda: list(ke_cutoffs_eV) 
+    )
+    deltas: List = field(default_factory=lambda: [])
+    integral_val: List = field(default_factory=lambda: [])
+    norm_inf: List = field(default_factory=lambda: [])
+    norm: List = field(default_factory=lambda: [])
+
+
+#sigmas = {4**0.5: Results(), 6**0.5: Results(), 8**0.5: Results(), 10**0.5: Results()}
+sigma_vals = [1, 4, 6, 10]
+sigmas = {s: Results() for s in sigma_vals} 
+#sigmas = {4: Results()}
+
+for sigma_k, res_dict in sigmas.items():
+    for ecut_ev in res_dict.cutoffs:
+        ecut_ha = ecut_ev / Hartree
+        nmax = get_ngmax(ecut_ha, L_bohr)
+        kgrid = np.arange(-nmax/2, nmax/2)
+        grid_spacing = 2 * np.pi / L_bohr
+        kxyz_grid = grid_spacing * kgrid
+        kpsq = (kxyz_grid + kproj[0]) ** 2.0
+        ksq = kxyz_grid**2.0
+        _prefactor = np.sqrt(2 * np.pi) / (sigma_k * L_bohr)
+        gaussian = np.exp(-ksq / (2 * sigma_k**2.0))
+        sum_k = np.sum(kpsq * gaussian)
+        prefactor = 1.0 / np.sum(gaussian)
+        ke_sum = sum_k * prefactor / (2 * mass_proj)
+        ke_int = sigma_k**2.0 / (2 * mass_proj)
+        res_dict.deltas.append(ke_sum - ke_int)
+        res_dict.norm.append(prefactor)
+        res_dict.norm_inf.append(_prefactor)
+        res_dict.integral_val.append(ke_int)
+        plt.plot(kxyz_grid, kpsq*gaussian * prefactor, marker="o", label=f"{nmax}")
+        xs = np.linspace(-abs(kxyz_grid[0]), abs(kxyz_grid[0]), 200)
+        dg = abs(xs[0] - xs[1])
+        print("ecut = ", ecut_ev)
+        print("nmax = ", nmax)
+        print("min k = ", min(grid_spacing*kgrid))
+        print("ke exact = ", ke_int)
+        print("ke sum= ", ke_sum)
+        print("norm exact = ", _prefactor)
+        print("norm sum= ", prefactor)
+        print("sigma = ", sigma_k)
+        # print(ecut_ev, L_bohr/nmax, sigma_k, ke_sum-ke_int, ke_int-sum(xs**2.0*np.exp(-xs**2.0/(2*sigma_k**2.0))*_prefactor/grid_spacing*dg)/(2*mass_proj), prefactor, _prefactor)
+        if ecut_ev == 1e5:
+            plt.plot(xs, xs**2.0*np.exp(-xs**2.0/(2*sigma_k**2.0))*_prefactor, marker="o", label="continuous")
+            plt.plot(xs, np.exp(-xs**2.0/(2*sigma_k**2.0))*_prefactor, marker="o", label="continuous")
+    plt.legend()
+    plt.savefig(f"gaussian_fig_{sigma_k}.pdf")
+    plt.cla()
+
+
+plt.cla()
+for i, sigma_k in enumerate(list(sigmas.keys())[::-1]):
+    plt.plot(
+        sigmas[sigma_k].cutoffs,
+        np.abs(sigmas[sigma_k].deltas),
+        marker="o",
+        label=rf"$\sigma_k$ = {sigma_k}",
+        color=colors[i],
+    )
+plt.tick_params(which="both", labelsize=14, direction="in")
+plt.legend(frameon=False, fontsize=14)
+plt.ylim([1e-11, 1e-1])
+plt.xlabel(r"$E_{\mathrm{cut}} (\mathrm{eV})$", fontsize=14)
+plt.yscale("log")
+plt.xscale("log")
+plt.ylabel("Kinetic Energy Error (Ha)", fontsize=14)
+plt.savefig("cutoff_convergence_1d.pdf", bbox_inches="tight", dpi=300)
+
+# for sigma_k, res_dict in sigmas.items():
+#     for ecut_ev in res_dict.cutoffs:
+#         ecut_ha = (ecut_ev/Hartree)
+#         nmax = get_ngmax(ecut_ha, L_bohr)
+#         kgrid = np.arange(-nmax//2, nmax//2)
+#         grid_spacing = 2*np.pi / L_bohr
+#         kxyz_grid = grid_spacing * cartesian_prod([kgrid, kgrid, kgrid])
+#         kpsq = np.sum((kxyz_grid + kproj[None,:])**2.0, axis=-1)
+#         ksq = np.sum(kxyz_grid**2.0, axis=-1)
+#         _prefactor = (np.sqrt(2*np.pi)/(sigma_k*L_bohr))**3.0
+#         gaussian = np.exp(-ksq/(2*sigma_k**2.0))
+#         sum_k = np.sum(kpsq*gaussian)
+#         prefactor = 1.0 / np.sum(gaussian)
+#         ke_sum = sum_k*prefactor/(2*mass_proj)# - ke
+#         ke_int = 3*sigma_k**2.0/(2*mass_proj)
+#         res_dict.deltas.append(ke_sum - ke_int)
+#         res_dict.norm.append(prefactor)
+#         res_dict.norm_inf.append(_prefactor)
+#         res_dict.integral_val.append(ke_int)
+#         print(sigma_k, ecut_ev, nmax, ke_sum-ke_int, ke + ke_int, prefactor - _prefactor)
--- notebooks/projectile_grid_cutoff_selection.py
+++ notebooks/projectile_grid_cutoff_selection.py
@@ -41,30 +41,40 @@ def main():
     fig, ax = plt.subplots(nrows=1, ncols=1)
     # set up grid number of points along a single dimension
     # 2 * pi * x / L
-    nx_grid = 2**np.arange(2, 6)  # quantum algorithm takes things as a power of 2
-    ke_cutoffs_eV = (2 * np.pi)**2 * nx_grid**2 / L**2 * 27.11 # highest energy components in eV
-    print(ke_cutoffs_eV)
+    nx_grid = 2**np.arange(2, 8)  # quantum algorithm takes things as a power of 2
+    ke_cutoffs_eV = 0.5 * (2 * np.pi)**2 * nx_grid**2 / L**2 * 27.11 # highest energy components in eV
+    # print(ke_cutoffs_eV)
+    # print(nx_grid)
     # variance for gaussian
-    sigma_squared = [1, 2, 4, 6]
+    sigma_squared = [1**2, 4**2, 6**2, 10**2]
     for idx, ss in enumerate(sigma_squared):
         k2_expectation_diff = []
-        for nx in nx_grid:
+        for ix, nx in enumerate(nx_grid):
             # note the grid is indexed by |p>
             kgrid = np.arange(-nx/2, nx/2)
-            norm_constant = 1 / (np.sqrt(2 * np.pi) * np.sqrt(ss))
-            ygrid = norm_constant * np.exp(-0.5 * kgrid**2 / ss)
+            _norm_constant = 1 / (np.sqrt(2 * np.pi) * np.sqrt(ss)) * 2*np.pi / L
+            factor = 2*np.pi / L
+            norm_constant = 1.0/np.sum(np.exp(-0.5 * (factor*kgrid)**2 / ss))
+            ygrid = norm_constant * np.exp(-0.5 * (factor*kgrid)**2 / ss)
 
             # grid spacing is 1.
-            discrete_rho_xsquared = np.sum(ygrid * kgrid**2) * (2 * np.pi)**2 / L**2 / (2 * m)  # extra (2pi)^2 / L^2 is for converting from int to wavenumber
-            exact_rho_xsquared = ss * (2 * np.pi)**2 / L**2 / (2 * m)
-            print(discrete_rho_xsquared, exact_rho_xsquared)
-            k2_expectation_diff.append(np.abs(np.sqrt(discrete_rho_xsquared) - np.sqrt(exact_rho_xsquared)) * 1000) # times 1K for milliHartree
-        print(k2_expectation_diff)
-        ax.plot(ke_cutoffs_eV, k2_expectation_diff, color=colors[idx], label=r"$\sigma = {}$".format(ss), marker='o')
+            discrete_rho_xsquared = np.sum(ygrid * (factor*kgrid)**2) / (2 * m)  # extra (2pi)^2 / L^2 is for converting from int to wavenumber
+            exact_rho_xsquared = ss / (2 * m)
+            # print("ecut = ", ke_cutoffs_eV[ix])
+            # print("nmax = ", nx)
+            # print("min k = ", min(factor*kgrid))
+            # print("ke exact = ", exact_rho_xsquared)
+            # print("ke sum= ", discrete_rho_xsquared)
+            # print("norm exact = ", _norm_constant/2)
+            # print("norm sum= ", norm_constant)
+            # print("sigma = ", ss)
+            k2_expectation_diff.append(np.abs(discrete_rho_xsquared - exact_rho_xsquared)) # times 1K for milliHartree
+        #print(k2_expectation_diff)
+        ax.plot(ke_cutoffs_eV, k2_expectation_diff, color=colors[idx], label=r"$\sigma^2 = {}$".format(ss), marker='o')
         
     ax.tick_params(which='both', labelsize=14, direction='in')
     ax.set_xlabel("$E_{cut}$ [eV]", fontsize=14)
-    ax.set_ylabel(r"Projectile Kinetic Energy Error [mHa]", fontsize=14)
+    ax.set_ylabel(r"Projectile Kinetic Energy Error [Ha]", fontsize=14)
     ax.tick_params(which='both', labelsize=14, direction='in')
     ax.legend(loc='lower left', fontsize=10, ncol=1, frameon=False)
     ax.set_title("One Dimensional Gaussian Kinetic Energy standard Error")
@@ -72,70 +82,70 @@ def main():
     ax.set_yscale("log")
     plt.gcf().subplots_adjust(bottom=0.15, left=0.2)
     plt.savefig("one_D_Gaussian_Variance_convergence.png", format='PNG', dpi=300)
-    plt.show()
+    #plt.show()
 
-    sigma_squared = 2
-    nx = 2**7
-    kgrid = np.arange(-nx/2, nx/2)
-    norm_constant = 1 / (np.sqrt(2 * np.pi) * np.sqrt(sigma_squared))
-    ygrid = norm_constant * np.exp(-0.5 * kgrid**2 / sigma_squared)
+    # sigma_squared = 2
+    # nx = 2**7
+    # kgrid = np.arange(-nx/2, nx/2)
+    # norm_constant = 1 / (np.sqrt(2 * np.pi) * np.sqrt(sigma_squared))
+    # ygrid = norm_constant * np.exp(-0.5 * kgrid**2 / sigma_squared)
 
-    fig, ax = plt.subplots(nrows=1, ncols=1)
-    ax.plot(kgrid, ygrid)
-    plt.savefig("gaussian.png", format='PNG', dpi=300)
+    # fig, ax = plt.subplots(nrows=1, ncols=1)
+    # ax.plot(kgrid, ygrid)
+    # plt.savefig("gaussian.png", format='PNG', dpi=300)
 
 
    # 3-D Problem
-    fig, ax = plt.subplots(nrows=1, ncols=1)
-    ke_cutoffs_eV = 3 * (2 * np.pi)**2 * nx_grid**2 / L**2 * 27.11
-    print(ke_cutoffs_eV)
-    sigma_squared = [1, 2, 4, 6]
-    for idx, ss in enumerate(sigma_squared):
-        k2_expectation_diff = []
-        for nx in nx_grid:
-            kgrid = np.arange(-nx/2, nx/2)
-            nxyz_grid = cartesian_prod([kgrid, kgrid, kgrid])
-            norm_constant = 1 / (np.sqrt(2 * np.pi)**3 * np.sqrt(ss)**3)
-            ygrid = norm_constant * np.exp(-0.5 * np.sum(nxyz_grid**2, axis=-1) / ss)
+    # fig, ax = plt.subplots(nrows=1, ncols=1)
+    # ke_cutoffs_eV = 3 * (2 * np.pi)**2 * nx_grid**2 / L**2 * 27.11
+    # print(ke_cutoffs_eV)
+    # sigma_squared = [1, 2, 4, 6]
+    # for idx, ss in enumerate(sigma_squared):
+    #     k2_expectation_diff = []
+    #     for nx in nx_grid:
+    #         kgrid = np.arange(-nx/2, nx/2)
+    #         nxyz_grid = cartesian_prod([kgrid, kgrid, kgrid])
+    #         norm_constant = 1 / (np.sqrt(2 * np.pi)**3 * np.sqrt(ss)**3)
+    #         ygrid = norm_constant * np.exp(-0.5 * np.sum(nxyz_grid**2, axis=-1) / ss)
 
-            discrete_rho_xsquared = np.sum(ygrid * np.sum(nxyz_grid**2, axis=-1)) * (2 * np.pi)**2 / L**2 / (2 * m)
-            exact_rho_xsquared = 3 * ss * (2 * np.pi)**2 / L**2 / (2 * m)
-            k2_expectation_diff.append(np.abs(np.sqrt(discrete_rho_xsquared) - np.sqrt(exact_rho_xsquared)) * 1000) # times 1K for milliHartree
-            print(np.sum(ygrid))
-        print(k2_expectation_diff)
-        ax.plot(ke_cutoffs_eV, k2_expectation_diff, color=colors[idx], label=r"$\sigma = {}$".format(ss), marker='o')
+    #         discrete_rho_xsquared = np.sum(ygrid * np.sum(nxyz_grid**2, axis=-1)) * (2 * np.pi)**2 / L**2 / (2 * m)
+    #         exact_rho_xsquared = 3 * ss * (2 * np.pi)**2 / L**2 / (2 * m)
+    #         k2_expectation_diff.append(np.abs(np.sqrt(discrete_rho_xsquared) - np.sqrt(exact_rho_xsquared)) * 1000) # times 1K for milliHartree
+    #         print(np.sum(ygrid))
+    #     print(k2_expectation_diff)
+    #     ax.plot(ke_cutoffs_eV, k2_expectation_diff, color=colors[idx], label=r"$\sigma = {}$".format(ss), marker='o')
         
-    ax.tick_params(which='both', labelsize=14, direction='in')
-    ax.set_xlabel("$E_{cut}$ [eV]", fontsize=14)
-    ax.set_ylabel(r"Projectile Kinetic Energy Error [mHa]", fontsize=14)
-    ax.tick_params(which='both', labelsize=14, direction='in')
-    ax.legend(loc='lower left', fontsize=10, ncol=1, frameon=False)
-    ax.set_title("Three Dimensional Gaussian Kinetic Energy standard Error")
-    ax.set_xscale("log")
-    ax.set_yscale("log")
-    plt.gcf().subplots_adjust(bottom=0.15, left=0.2)
-    plt.savefig("three_D_Gaussian_Variance_convergence.png", format='PNG', dpi=300)
-    plt.show()
+    # ax.tick_params(which='both', labelsize=14, direction='in')
+    # ax.set_xlabel("$E_{cut}$ [eV]", fontsize=14)
+    # ax.set_ylabel(r"Projectile Kinetic Energy Error [mHa]", fontsize=14)
+    # ax.tick_params(which='both', labelsize=14, direction='in')
+    # ax.legend(loc='lower left', fontsize=10, ncol=1, frameon=False)
+    # ax.set_title("Three Dimensional Gaussian Kinetic Energy standard Error")
+    # ax.set_xscale("log")
+    # ax.set_yscale("log")
+    # plt.gcf().subplots_adjust(bottom=0.15, left=0.2)
+    # plt.savefig("three_D_Gaussian_Variance_convergence.png", format='PNG', dpi=300)
+    # plt.show()
 
-    nx_grid = 2**np.arange(3, 8)
-    sigma_squared = 4
-    for nx_grid_val in nx_grid:
-        sbp = SquareBoxPlanewaves(a, nx_grid_val)
-        p_basis = sbp.get_p_basis()
-        kp = sbp.get_kp_basis()
-        ke_vals = np.sum(kp**2, axis=-1) / (2 * m)
-        ke_val_test = sbp.get_ke_diagonal_values(mass=m)
-        assert np.allclose(ke_vals, ke_val_test)
-        print("Max Ke-proj x-direction ", ke_vals[0])
-        print("Max Ke-e x-direction ", np.sum(kp[0]**2) / 2 * 27.11) 
-        print("Total Planewaves ", sbp.N)
+    # nx_grid = 2**np.arange(3, 8)
+    # sigma_squared = 4
+    # for nx_grid_val in nx_grid:
+    #     sbp = SquareBoxPlanewaves(a, nx_grid_val)
+    #     p_basis = sbp.get_p_basis()
+    #     kp = sbp.get_kp_basis()
+    #     ke_vals = np.sum(kp**2, axis=-1) / (2 * m)
+    #     ke_val_test = sbp.get_ke_diagonal_values(mass=m)
+    #     assert np.allclose(ke_vals, ke_val_test)
+    #     print("Max Ke-proj x-direction ", ke_vals[0])
+    #     print("Max Ke-e x-direction ", np.sum(kp[0]**2) / 2 * 27.11) 
+    #     print("Total Planewaves ", sbp.N)
 
-        sigma = np.sqrt(sigma_squared)
-        norm_constant = 1 / ((2 * np.pi)**(3/2) * sigma**(3))
-        prob_dist = norm_constant * np.exp(-np.sum(p_basis**2, axis=-1)/(2 * sigma_squared))
-        print(np.sum(prob_dist))
-        print(np.sum(prob_dist * ke_vals) / (2 * m))
-        print()
+    #     sigma = np.sqrt(sigma_squared)
+    #     norm_constant = 1 / ((2 * np.pi)**(3/2) * sigma**(3))
+    #     prob_dist = norm_constant * np.exp(-np.sum(p_basis**2, axis=-1)/(2 * sigma_squared))
+    #     print(np.sum(prob_dist))
+    #     print(np.sum(prob_dist * ke_vals) / (2 * m))
+    #     print()
 
 
 if __name__ == "__main__":
