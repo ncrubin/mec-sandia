b650a8a9bc36346da355c7ad30941bea6280b273
Fionn Malone <fmalone@google.com>
2023-06-29 23:13:18 +0000

    Update.

--- mec_sandia/ueg_test.py
+++ mec_sandia/ueg_test.py
@@ -17,17 +17,17 @@ def test_eris():
     eris_4 = ueg.eri_4()
     assert eris_4.shape == (19,) * 4
     # ERIS are stored like (ij|kl)
-    # for i, j, k, l in itertools.product(range(ueg.nbasis), repeat=4):
-    #     # momentum conservation
-    #     q1 = ueg.basis[k] - ueg.basis[i]
-    #     q2 = ueg.basis[j] - ueg.basis[l]
-    #     if np.allclose(q1, q2) and np.dot(q1, q1) > 0:
-    #         assert np.isclose(
-    #             eris_4[i, k, j, l],
-    #             1.0 / ueg.vol * ueg.vq(ueg.kfac * (ueg.basis[k] - ueg.basis[i])),
-    #         )
-    #     else:
-    #         assert eris_4[i, k, j, l] == 0
+    for i, j, k, l in itertools.product(range(ueg.nbasis), repeat=4):
+        # momentum conservation
+        q1 = ueg.basis[k] - ueg.basis[i]
+        q2 = ueg.basis[j] - ueg.basis[l]
+        if np.allclose(q1, q2) and np.dot(q1, q1) > 0:
+            assert np.isclose(
+                eris_4[i, k, j, l],
+                1.0 / ueg.vol * ueg.vq(ueg.kfac * (ueg.basis[k] - ueg.basis[i])),
+            )
+        else:
+            assert eris_4[i, k, j, l] == 0
 
     h1e = np.diag(ueg.sp_eigv)
     assert h1e.shape == (ueg.nbasis,) * 2
